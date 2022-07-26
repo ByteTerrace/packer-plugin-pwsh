559b2bd59b26f7feaad50fa1e0e42a1df1156cca
David Smith <kittoes0124@byteterrace.com>
2022-07-26 17:33:20 -0500

    attempting to fix batch syntax errors

--- provisioner/provisioner.go
+++ provisioner/provisioner.go
@@ -50,7 +50,7 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 	}
 
 	if p.config.ExecuteCommand == "" {
-		p.config.ExecuteCommand = "FOR /F \"tokens=* USEBACKQ\" %F IN (`where pwsh /R \"%PROGRAMFILES%\\PowerShell\" ^2^>nul ^|^| where powershell ^| tail --lines=1`) DO (SET command=start \"\" \"%F\" -ExecutionPolicy 'Bypass' -Command \"Write-Host 'Hello packer!'\")"
+		p.config.ExecuteCommand = "FOR /F \"tokens=* USEBACKQ\" %F IN (`where pwsh /R \"%PROGRAMFILES%\\PowerShell\" ^2^>nul ^|^| where powershell ^| tail --lines=1`) DO (SET command=start \"\" \"%F\" -ExecutionPolicy \"Bypass\" -Command \"Write-Host 'Hello packer!'\"); && %command%"
 	}
 
 	return nil
