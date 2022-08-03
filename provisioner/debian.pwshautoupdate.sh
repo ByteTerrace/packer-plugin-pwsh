6e8ed6a25e742eb6a90b8c3fc25be9cb21bd2c18
David Smith <kittoes0124@byteterrace.com>
2022-08-02 23:02:09 -0500

    refuctoring?

--- provisioner/debian.pwshautoupdate.sh
+++ provisioner/debian.pwshautoupdate.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-wget --output-document "/tmp/packer-pwsh-installer.deb" "{{.Uri}}"
+wget --output-document "/tmp/packer-pwsh-installer.deb" "{{.Uri}}" --output-file /dev/null
 dpkg -i "/tmp/packer-pwsh-installer.deb"
